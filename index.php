34bc95e9369f6df8887c8bb9d0ee44736964bf17
Jordan2811 <61702294+Jordan2811@users.noreply.github.com>
2020-04-01 17:55:15 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once 'Truck.php';
+$truck = new Truck('blue', 4, 'fuel', 50);
+echo $truck->setLoad(10) . ' ';
+echo $truck->fullOrNot() . '<br>';
+echo $truck->setLoad(70) . ' ';
+echo $truck->fullOrNot() . '<br>';
+$truck = new Truck('red', 3, 'electric', 30);
+echo $truck->setLoad(-10) . ' ';
+echo $truck->fullOrNot() . '<br>';
+echo $truck->setLoad(70) . ' ';
+echo $truck->fullOrNot();
+echo $truck->brake();
+echo $truck->forward();
---
+++ truck.php
@@ -0,0 +1,60 @@
+<?php
+
+require_once 'Vehicle.php';
+
+class Truck extends Vehicle
+{
+    private $energy;
+    private $storageCapacity;
+    private $load = 0;
+
+     public const MIN_LOAD = 0;
+     public function __construct(strin $color, int $nbSeats, string $energy, int $storageCapacity)
+     {
+         parent::__construct($color, $nbSeats);
+         $this->setEnergy($energy);
+         $this->setStorageCapacity($storageCapacity);
+     }
+
+     public function getEnergy(): string
+     {
+         return $this->energy;
+     }
+
+    public function setEnergy(string $energy): truck
+    {
+        if (in_array($energy, self::ALLOWED_ENERGIES)) {
+            $this->energy = $energy;
+        }
+        return $this;
+    }
+
+
+    public function getStorageCapacity(int $storageCapacity)
+    {
+        return $this->storageCapacity = $storageCapacity;
+    }
+
+    public function getLoad()
+    {
+        return $this->load;
+    }
+
+
+    public function setLoad(int $load)
+    {
+        if ($load < 0 and $load < 50) {
+            return $this->load = self::MIN_LOAD;
+        }
+        return $this->>load = $load;
+    }
+
+    public function fullOrNot()
+    {
+        if ($this->load == 50){
+            return 'full';
+        } else {
+            return 'in filling';
+        }
+    }
+}
