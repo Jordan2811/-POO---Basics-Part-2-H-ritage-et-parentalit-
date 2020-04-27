eaea2b1e6bf5fa9fa7e542549f8ada19c9ba1e1b
Jordan2811 <61702294+Jordan2811@users.noreply.github.com>
2020-04-27 10:54:52 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,28 @@
 <?php
 
 
-require_once 'truck.php';
-$truck = new Truck('blue', 4, 'fuel', 50);
-echo $truck->setLoad(10) . ' ';
-echo $truck->fullOrNot() . '<br>';
-echo $truck->setLoad(70) . ' ';
-echo $truck->fullOrNot() . '<br>';
-$truck = new Truck1('red', 3, 'electric', 30);
-echo $truck1->fullOrNot() . '<br>';
-echo $truck1->setLoad(70) . ' ';
-echo $truck1->fullOrNot();
-echo $truck1->brake();
-echo $truck1->forward();
+require_once 'Bicycle.php';
+require_once 'Car.php';
+require_once 'Truck.php';
+
+
+
+
+$bicycle = new Bicycle('blue', 1);
+echo $bicycle->forward();
+var_dump($bicycle);
+
+$car = new Car('green', 4, 'electric');
+echo $car->forward();
+var_dump($car);
+
+var_dump(Car::ALLOWED_ENERGIES);
+
+$truck = new Truck('Black','3','diesel','50','100');
+echo $truck->getStorageCapacity();
+$truck->setStorageCapacity(0);
+echo $truck->getStorageCapacity();
+$truck->setStorageCapacity(90);
+echo $truck->getStorageCapacity();
+$truck->setStorageCapacity(121);
+echo $truck->getStorageCapacity();
