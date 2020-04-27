1fe46b8c9d3b70371be4b83644cf471fc4d28be5
Jordan2811 <61702294+Jordan2811@users.noreply.github.com>
2020-04-27 10:53:18 +0200

    Update truck.php

--- truck.php
+++ truck.php
@@ -2,59 +2,61 @@
 
 require_once 'Vehicle.php';
 
-class truck extends Vehicle
+class Truck extends Vehicle
 {
-    private $energy;
-    private $storageCapacity;
-    private $load = 0;
+	const ALLOWED_ENERGIES = [
+		'fuel',
+		'electric',
+		'diesel',
+];
+	const MIN_STORAGE = 0;
 
-     public const MIN_LOAD = 0;
-     public function __construct(string $color, int $nbSeats, string $energy, int $storageCapacity)
-     {
-         parent::__construct($color, $nbSeats);
-         $this->setEnergy($energy);
-         $this->StorageCapacity = $storageCapacity;
-     }
+	/**
+	 * @var string
+	 */
+	private $tenergy;
 
-     public function getEnergy(): string
-     {
-         return $this->energy;
-     }
+	/**
+	 * @var int
+	 */
+	private $energyLevel;
 
-    public function setEnergy(string $energy): truck
-    {
-        if (in_array($energy, self::ALLOWED_ENERGIES)) {
-            $this->energy = $energy;
-        }
-        return $this;
-    }
+	/**
+	 * @var int
+	 */
+	private $storageCapacity = 0;
+	private $storageMaxCapacity = 100;
+
+	public function __construct(string $color, int $nbSeats, string $tenergy,int $storageCapacity,int $storageMaxCapacity)
+	{
+		parent::__construct($color,$nbSeats);
+		$this->tenergy = $tenergy;
+		$this->storageCapacity = $storageCapacity;
+		$this->storageMaxCapacity = $storageMaxCapacity;
+	}
+
+	public function getEnergyLevel(): int
+	{
+		return $this->energyLevel;
+	}
+
+	public function setEnergyLevel(int $energyLevel): void
+	{
+		$this->energyLevel = $energyLevel;
+	}
+
+	public function setStorageCapacity(int $storageCapacity): void
+	{
+		$this->storageCapacity = $storageCapacity;
+	}
+
+	public function getStorageCapacity():string  {
+		if ($this->storageCapacity > $this->storageMaxCapacity){
+			return "The vehicule is Overstorage!"."<br>";
+		} elseif ($this->storageCapacity <= self::MIN_STORAGE){
+			return "The vehicule is empty !"."<br>";
+		} return "The truck is in filling at ".$this->storageCapacity. " tonnes."."<br>";
+	}
 
 
-    public function getStorageCapacity(int $storageCapacity)
-    {
-        return $this->storageCapacity = $storageCapacity;
-    }
-
-    public function getLoad()
-    {
-        return $this->load;
-    }
-
-
-    public function setLoad(int $load)
-    {
-        if ($load < 0 and $load < 50) {
-            return $this->load = self::MIN_LOAD;
-        }
-        return $this->load = $load;
-    }
-
-    public function fullOrNot()
-    {
-        if ($this->load == 50){
-            return 'full';
-        } else {
-            return 'in filling';
-        }
-    }
 }
